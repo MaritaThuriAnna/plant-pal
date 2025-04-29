54523a4966dacf4648e834bd51da58575164ecb7
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-04-29 13:39:48 +0300

    dashboard created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Store } from '@ngrx/store';
 import { loadPlants } from './store/plants/plant.actions';
 import { AddPlantComponent } from "./components/add-plant/add-plant.component";
 import { PlantListComponent } from "./components/plant-list/plant-list.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
-  imports: [AddPlantComponent, PlantListComponent],
+  imports: [AddPlantComponent, PlantListComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
